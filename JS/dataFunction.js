59c6713f0a4a6d273063a6d3fe9bbe4efaf6f3cb
coumbalee <coumba.ly@yahoo.com>
2021-09-12 03:00:00 +0200

    Création et export fonction qui récupèrent données json

---
+++ JS/dataFunction.js
@@ -0,0 +1,34 @@
+"use strict";
+// fonction qui récupère les données du fichier json
+export async function fetchData() {
+  let response = await fetch("./../public/data.json");
+  let data = await response.json();
+  console.log(data);
+  return data;
+}
+// fonction qui récupère les photographes du fichier json
+export async function getPhotographersFromJson() {
+  const data = await fetchData();
+  console.log(data.photographers);
+
+  return data.photographers;
+}
+
+console.log(getPhotographersFromJson);
+
+// fonction qui récupère les médias du fichier json
+export async function getMediaFromJson() {
+  const data = await fetchData();
+  console.log(data.media);
+  return data.media;
+}
+// fonction qui récupère les tags des photographes
+export function getAllTagsFromPhotographers(photographers) {
+  let allTags = [];
+  for (const photographer of photographers) {
+    allTags = allTags.concat(photographer.tags);
+  }
+  const tagsSet = new Set(allTags);
+  const tags = [...tagsSet];
+  return tags;
+}
