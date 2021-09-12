65d842675b267715b4345cc955d44d3becfa8abc
coumbalee <coumba.ly@yahoo.com>
2021-09-12 02:59:09 +0200

    import fonctions dataFunction et création éléments header

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,36 @@
-fetch("http://192.168.0.26:5500/public/data.json")
-  .then((response) => response.json())
-  .then((response) => console.log(response));
+"use strict";
+import {
+  fetchData,
+  getPhotographersFromJson,
+  getAllTagsFromPhotographers,
+  getMediaFromJson,
+} from "./dataFunction.js";
 
-fetch("data.json")
-  .then((response) => response.json())
-  .then((json) => console.log(response));
+getPhotographersFromJson().then((photographers) => {
+  const tags = getAllTagsFromPhotographers(photographers);
+  console.log(tags);
+  let tagsElt = document.querySelector("#tags");
+  tags.forEach((tag) => {
+    tagsElt.innerHTML += `<span data-tag="${tag}">#${tag}</span>`;
+  });
+  document.querySelectorAll("#tags span").forEach((span) => {
+    span.addEventListener("click", (e) => {
+      alert(e.target.dataset.tag);
+    });
+  });
+});
+
+// création du lien du haut du header
+const headerLinkTop = document.querySelector(".header");
+headerLinkTop.querySelector(".header__link--top").innerHTML =
+  "Passer au contenu";
+
+// création du lien 'nos photographes' du header
+const headerLinkRight = document.querySelector(".header");
+headerLinkRight.querySelector(".header__link--right").innerHTML =
+  "Nos photographes";
+
+// insertion du logo
+const img = document.createElement("img");
+img.src = "/IMAGES/logo.png";
+document.querySelector(".header__logo").appendChild(img);
