3c65e3d21a0306bfaed1666065a8fb2ef74597bd
coumbalee <coumba.ly@yahoo.com>
2021-10-25 01:25:13 +0200

    class imageMedia+ fonction displayInList

---
+++ JS/imageMedia.js
@@ -0,0 +1,29 @@
+import { imageExist } from "./functions.js";
+export default class imageMedia {
+  constructor(media) {
+    this.id = media.id;
+    this.photographerId = media.photographerId;
+    this.title = media.title;
+    this.image = media.image;
+    this.tags = media.tags;
+    this.likes = media.likes;
+    this.date = media.date;
+    this.price = media.price;
+  }
+  displayInList(baseUrl) {
+    return imageExist(baseUrl + "/" + this.image)
+      ? `
+    <li class="media-section__card"  >
+    <img src="${baseUrl}/${this.image}"  class="mediaImg" >
+    <div class ="media-section__content">
+    <h2 class ="media-section__title">${this.title}</h2>
+    <div class ="media-section__likes">
+    <p class="media-section__number">${this.likes}</p>
+    <i class="fas fa-heart"></i>
+    </div>
+    </div>
+    </li>
+    `
+      : undefined;
+  }
+}
