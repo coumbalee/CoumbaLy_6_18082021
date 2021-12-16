d608685b78dd526341cd5bbff0e36607fb20a8a9
coumbalee <coumba.ly@yahoo.com>
2021-12-16 15:53:34 +0100

    test function focusOnModal

--- JS/functions.js
+++ JS/functions.js
@@ -381,6 +381,7 @@ export function generateForm(photographer) {
     "click",
     function (event) {
       checkInputs();
+      focusOnModal();
       event.preventDefault();
     },
     false
@@ -595,4 +596,40 @@ export function showPrice(photographer) {
   const price = document.querySelector("#price");
   console.log(price);
   price.innerHTML = `${photographer.price} / jour`;
+}
+function focusOnModal() {
+  const focusableElements =
+    'span, h1, input, textarea, button,[tabindex]:not([tabindex="-1"])';
+  const modal = document.querySelector(".form");
+  console.log(modal);
+
+  const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+  console.log(firstFocusableElement);
+  const focusableContent = modal.querySelectorAll(focusableElements);
+  const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+  document.addEventListener("keydown", function (e) {
+    let isTabPressed = e.key === "Tab" || e.keyCode === 9;
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) {
+      // if shift key pressed for shift + tab combination
+      if (document.activeElement === firstFocusableElement) {
+        lastFocusableElement.focus(); // add focus for the last focusable element
+        e.preventDefault();
+      }
+    } else {
+      // if tab key is pressed
+      if (document.activeElement === lastFocusableElement) {
+        // if focused has reached to last focusable element then focus first focusable element after pressing tab
+        firstFocusableElement.focus(); // add focus for the first focusable element
+        e.preventDefault();
+      }
+    }
+  });
+
+  firstFocusableElement.focus();
 }
