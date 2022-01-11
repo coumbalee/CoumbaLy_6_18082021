d9f8e17253f75ff55d739753b0fdc636246eb6d6
coumbalee <coumba.ly@yahoo.com>
2022-01-11 15:33:48 +0100

    Ajout this.altText au constructor

--- JS/imageMedia.js
+++ JS/imageMedia.js
@@ -9,17 +9,18 @@ export default class imageMedia {
     this.likes = media.likes;
     this.date = media.date;
     this.price = media.price;
+    this.altText = media.altText;
   }
   displayInList(baseUrl) {
     return imageExist(baseUrl + "/" + this.image)
       ? `
     <li class="media-section__card" data-id="${this.id}" >
-    <img src="${baseUrl}/${this.image}"  class="mediaImg" tabindex="0" alt="image ${this.title}">
+    <img src="${baseUrl}/${this.image}"  class="mediaImg" tabindex="0" alt="${this.altText}">
     <div class ="media-section__content">
     <h2 class ="media-section__title " tabindex="0">${this.title} </h2>
     <div class ="media-section__likes" tabindex="0">
     <p class="media-section__number">${this.likes}</p>
-    <span class="fas fa-heart media-section__heart" aria-label="button like" role="button" ></span>
+    <span class="fas fa-heart media-section__heart" aria-label="bouton j'aime" role="button" ></span>
     </div>
     </div>
     </li>
--- JS/videoMedia.js
+++ JS/videoMedia.js
@@ -9,20 +9,21 @@ export default class videoMedia {
     this.likes = media.likes;
     this.date = media.date;
     this.price = media.price;
+    this.altText = media.altText;
   }
   displayInList(baseUrl) {
     return imageExist(baseUrl + "/" + this.video)
       ? `
     <li class="media-section__card" data-id="${this.id}">
-    <video tabindex="0">
-    <source src="${baseUrl}/${this.video}" class="mediaVideo" alt="vidéo ${this.title}"
+    <video tabindex="0" title="${this.altText}">
+    <source src="${baseUrl}/${this.video}"   class="mediaVideo"  
     >
     </video>
     <div class ="media-section__content">
     <h2 class ="media-section__title" tabindex="0">${this.title}</h2>
     <div class ="media-section__likes" tabindex="0">
     <p class="media-section__number">${this.likes}</p>
-    <span class="fas fa-heart" role="button" aria-label="button like"></span>
+    <span class="fas fa-heart" role="button" aria-label="bouton j'aime"></span>
     </div>
     </div>
     </li>
